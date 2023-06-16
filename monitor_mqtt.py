ae6b9fcdf434790debc635354bde0fa7220d7815
Nettking <m@nettking.no>
2023-06-16 17:59:46 +0200

    Update monitor_mqtt.py

--- monitor_mqtt.py
+++ monitor_mqtt.py
@@ -1,22 +1,5 @@
 import subprocess
 
-try:
-    import paho.mqtt.client as mqtt
-except:
-    def is_paho_installed():
-        installed_packages = subprocess.check_output(["pip", "list"]).decode("utf-8")
-        return 'paho-mqtt' in installed_packages
-
-    def install_paho():
-        subprocess.check_call(["pip", "install", "paho-mqtt"])
-
-    if not is_paho_installed():
-        print("paho-mqtt is not installed. Installing now...")
-        install_paho()
-        print("Installation completed.")
-    else:
-        print("paho-mqtt is already installed.")
-    import paho.mqtt.client as mqtt
 
 def on_message(client, userdata, message):
     # Decode the message payload from bytes to string
@@ -43,28 +26,34 @@ def establish_connection(MQTT_BROKER_ADDR, MQTT_BROKER_PORT, MQTT_TOPIC_SUB, MQT
 
     # Set up the callback function for receiving messages
     client.on_message = on_message
-
-    #message = f'{{"temperature":{{"id":1,"txt":"temperature","t":{test_temp}}}}}'
-    #message_lum = f'{{"luminance":{{"id":1,"txt":{input_luminance}}}}}'
-    # Publish a message to the specified topic with the retain flag set to False and QoS level 1
-    #client.publish(MQTT_TOPIC_PUB, message, retain=False, qos=1)
-    #client.publish(MQTT_TOPIC_PUB, message_lum, retain=False, qos=1)
-    #print("Published message: " + message)
     
     client.loop_forever()
 
 
 if __name__ == "__main__":
+   
+    try:
+        import paho.mqtt.client as mqtt
+    except:
+        def is_paho_installed():
+            installed_packages = subprocess.check_output(["pip", "list"]).decode("utf-8")
+            return 'paho-mqtt' in installed_packages
 
+        def install_paho():
+            subprocess.check_call(["pip", "install", "paho-mqtt"])
+
+        if not is_paho_installed():
+            print("paho-mqtt is not installed. Installing now...")
+            install_paho()
+            print("Installation completed.")
+            import paho.mqtt.client as mqtt
+        else:
+            print("paho-mqtt is already installed.")
 
     host = input("Please enter host (openhabian) IP: ")
     port = 1883
-    #test_temp = input("Please enter the temperature to send: ")
-    #input_luminance = input("Please enter the luminance to send: ")
     topic_temp = 'CPS2021/tempoutput'
     topic_switch = 'CPS2021/SwitchControl'
-
-
-
+    
     establish_connection(host, port, topic_switch, topic_temp)
         
