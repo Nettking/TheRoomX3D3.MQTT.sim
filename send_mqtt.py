557e5720e8a4906bb2f34fc8578356c58fed4f72
Nettking <m@nettking.no>
2023-06-16 17:59:43 +0200

    Update send_mqtt.py

--- send_mqtt.py
+++ send_mqtt.py
@@ -1,22 +1,6 @@
 import subprocess
 
-try:
-    import paho.mqtt.client as mqtt
-except:
-    def is_paho_installed():
-        installed_packages = subprocess.check_output(["pip", "list"]).decode("utf-8")
-        return 'paho-mqtt' in installed_packages
 
-    def install_paho():
-        subprocess.check_call(["pip", "install", "paho-mqtt"])
-
-    if not is_paho_installed():
-        print("paho-mqtt is not installed. Installing now...")
-        install_paho()
-        print("Installation completed.")
-    else:
-        print("paho-mqtt is already installed.")
-    import paho.mqtt.client as mqtt
 
 def on_message(client, userdata, message):
     # Decode the message payload from bytes to string
@@ -44,15 +28,30 @@ def establish_connection(MQTT_BROKER_ADDR, MQTT_BROKER_PORT, MQTT_TOPIC_SUB, MQT
 
 if __name__ == "__main__":
 
+    try:
+        import paho.mqtt.client as mqtt
+    except:
+        def is_paho_installed():
+            installed_packages = subprocess.check_output(["pip", "list"]).decode("utf-8")
+            return 'paho-mqtt' in installed_packages
 
+        def install_paho():
+            subprocess.check_call(["pip", "install", "paho-mqtt"])
+
+        if not is_paho_installed():
+            print("paho-mqtt is not installed. Installing now...")
+            install_paho()
+            print("Installation completed.")
+            import paho.mqtt.client as mqtt
+        else:
+            print("paho-mqtt is already installed.")
+        
     host = input("Please enter host (openhabian) IP: ")
     port = 1883
     test_temp = input("Please enter the temperature to send: ")
     input_luminance = input("Please enter the luminance to send: ")
     topic_temp = 'CPS2021/tempoutput'
     topic_switch = 'CPS2021/SwitchControl'
-
-
 
     establish_connection(host, port, topic_switch, topic_temp)
         
